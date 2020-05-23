327f850adf9b6dad7b05ce3bf7d0bb2b60ac0a35
Zachary Hislop <hislopzach@gmail.com>
2020-05-23 16:24:52 -0700

    add basic wind chart

---
+++ src/WindChart.js
@@ -0,0 +1,46 @@
+import React from "react";
+import {
+  LineChart,
+  CartesianGrid,
+  Line,
+  Tooltip,
+  XAxis,
+  YAxis,
+  ResponsiveContainer,
+} from "recharts";
+import { format, fromUnixTime } from "date-fns";
+import colors from "./colors";
+const styles = {
+  chartFillIn: {
+    height: 250,
+    width: "100%",
+  },
+};
+
+const formatTime = (timestamp) => {
+  return format(fromUnixTime(timestamp), "EEEEEE MM/dd h aaaa");
+};
+
+const WindChart = ({ surflineResponse, synchId, ...props }) => {
+  const data = surflineResponse.data.wind
+    .filter((wind, ndx) => ndx % 6 === 0 || ndx < 2)
+    .map((wind) => ({
+      humanTime: formatTime(wind.timestamp),
+      ...wind,
+    }));
+  return (
+    <div style={styles.chartFillIn}>
+      <ResponsiveContainer width="100%">
+        <LineChart data={data} synchId={synchId}>
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="humanTime" />
+          <YAxis />
+          <Tooltip />
+          <Line dataKey="speed" stroke={colors.blue} strokeWidth={5} />
+        </LineChart>
+      </ResponsiveContainer>
+    </div>
+  );
+};
+
+export default WindChart;
