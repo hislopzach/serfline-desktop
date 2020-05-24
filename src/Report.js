55071ce215abdf5b0fda04efa9fd063f0925c6e3
Zachary Hislop <hislopzach@gmail.com>
2020-05-23 18:33:04 -0700

    add basic report

---
+++ src/Report.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { Box, Grid, Typography } from "@material-ui/core";
+import { degToCompass } from "./util";
+
+const Report = ({ surflineResponse, ...props }) => {
+  const { spot, forecast } = surflineResponse;
+  return (
+    <Grid container justify="center" spacing={2}>
+      <Grid item xs="12">
+        <Typography variant="h2">{spot.name}</Typography>
+      </Grid>
+      <Grid item xs="auto">
+        Tide: {forecast.tide.current.height} ft
+      </Grid>
+      <Grid item xs="auto">
+        Wind: {forecast.wind.speed}kts {degToCompass(forecast.wind.direction)}
+      </Grid>
+      <Grid item xs="auto">
+        Surf Height: {forecast.waveHeight.min}-{forecast.waveHeight.max} ft
+      </Grid>
+    </Grid>
+  );
+};
+
+export default Report;
