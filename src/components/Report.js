69d1dd5b21d5bc3d412726bb0a1181fa5e35382a
Zachary Hislop <hislopzach@gmail.com>
2020-06-28 19:18:56 -0700

    add highlights

---
+++ src/components/Report.js
@@ -0,0 +1,57 @@
+import React from "react";
+import { Grid, Typography } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import { degToCompass } from "../util";
+
+const useStyles = makeStyles((theme) => ({
+  details: {
+    marginBottom: 5,
+  },
+  highlights: {
+    textAlign: "left",
+  },
+}));
+
+const Report = ({ report, overview, ...props }) => {
+  const classes = useStyles();
+  const { spot, forecast } = report;
+  const {
+    data: { forecastSummary },
+  } = overview;
+  return (
+    <Grid container justify="center">
+      <Grid item xs={12}>
+        <Typography variant="h2">{spot.name}</Typography>
+      </Grid>
+      <Grid item xs={6} className={classes.highlights}>
+        <strong>Highlights:</strong>
+        <ul>
+          {forecastSummary.highlights.map((highlight) => (
+            <li>{highlight}</li>
+          ))}
+        </ul>
+      </Grid>
+      <Grid
+        container
+        item
+        xs={6}
+        direction="column"
+        justify="space-around"
+        alignItems="center"
+      >
+        <Grid item className={classes.details}>
+          Tide: {forecast.tide.current.height} ft{" "}
+          {forecast.tide.next.type === "LOW" ? "Falling" : "Rising"}
+        </Grid>
+        <Grid item>
+          Wind: {forecast.wind.speed}kts {degToCompass(forecast.wind.direction)}
+        </Grid>
+        <Grid item>
+          Surf Height: {forecast.waveHeight.min}-{forecast.waveHeight.max} ft
+        </Grid>
+      </Grid>
+    </Grid>
+  );
+};
+
+export default Report;
