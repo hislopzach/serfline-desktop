24d3ebb22e6d743eeb265ea3a9c578a10500773e
Zachary Hislop <hislopzach@gmail.com>
2020-06-28 19:32:32 -0700

    use lineup as site title

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,12 +1,27 @@
 import React from "react";
-import { AppBar, Toolbar, Button } from "@material-ui/core";
+import { AppBar, Toolbar, Typography } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import colors from "../colors";
+
 import { Link } from "react-router-dom";
+
+const useStyles = makeStyles((theme) => ({
+  button: {
+    color: "white",
+  },
+  link: {
+    textDecoration: "none",
+  },
+}));
 const NavBar = () => {
+  const classes = useStyles();
   return (
     <AppBar>
       <Toolbar>
-        <Link to="/">
-          <Button>Line Hacker</Button>
+        <Link to="/" className={classes.link}>
+          <Typography variant="h5" className={classes.button}>
+            Lineup
+          </Typography>
         </Link>
       </Toolbar>
     </AppBar>
