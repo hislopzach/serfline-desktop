aeaba63e6b6d17efea26b0bd7806e82ddd2b31e4
Zach Hislop <zach@praevium.com>
2020-08-19 11:12:23 -0700

    remove unused import

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { AppBar, Toolbar, Typography } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
-import colors from "../colors";
 
 import { Link } from "react-router-dom";
 
